e8001d841693cb03e6630f203454045515f0c18c
burakkarakaya <burakkarakaya1984@gmail.com>
2024-04-21 19:28:12 +0300

    no result, avatar
    no result data
    add avatar

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,12 +1,28 @@
 <template>
     <ul class="data-list">
-        <li v-for="item in data" :key="item.id">
-            <a :href="item.html_url" target="_blank">{{ item.title }}</a>
-            <span>#{{item.number}} opened on {{formatDate(item.created_at)}} by <a target="_blank" :href="item.user.html_url">{{item.user.login}}</a></span>
-            <div class="labels">
-                <span v-for="label in item.labels" :key="label.id" :style="{ backgroundColor: '#' + label.color }">{{label.name}}</span>
-            </div>
-        </li>
+        <template v-if="data.length > 0">
+            <li v-for="item in data" :key="item.id">
+                
+                <div class="d-flex">
+
+                    <img class="avatar" :alt="item.user.login" :src="item.user.avatar_url" />
+
+                    <div>
+
+                        <a :href="item.html_url" target="_blank">{{ item.title }}</a>
+                        <span>#{{item.number}} opened on {{formatDate(item.created_at)}} by <a target="_blank" :href="item.user.html_url">{{item.user.login}}</a></span>
+                        <div class="labels">
+                            <span v-for="label in item.labels" :key="label.id" :style="{ backgroundColor: '#' + label.color }">{{label.name}}</span>
+                        </div>
+
+                    </div>
+
+                </div>
+            </li>
+        </template>
+        <template v-if="data.length == 0">
+            <li>No Result</li>
+        </template>
     </ul>
 </template>
 
